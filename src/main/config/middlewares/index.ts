666f135d207575473dccfeb8f6a3750d1fe7eaef
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-06-17 14:23:31 -0300

    feat: add helmet

--- src/main/config/middlewares/index.ts
+++ src/main/config/middlewares/index.ts
@@ -2,8 +2,10 @@ import type { Express } from 'express';
 
 import { setupBodyParser } from './bodyParser';
 import { setupCors } from './cors';
+import { setupHelmet } from './helmet';
 
 export function setupMiddlewares(app: Express) {
+  setupHelmet(app);
   setupBodyParser(app);
   setupCors(app);
 }
