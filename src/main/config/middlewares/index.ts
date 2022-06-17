a3601ca978c3ef666eb5b28b935fe469b5431093
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-06-17 14:21:37 -0300

    feat: add json body parser

--- src/main/config/middlewares/index.ts
+++ src/main/config/middlewares/index.ts
@@ -1,7 +1,9 @@
 import type { Express } from 'express';
 
+import { setupBodyParser } from './bodyParser';
 import { setupCors } from './cors';
 
 export function setupMiddlewares(app: Express) {
+  setupBodyParser(app);
   setupCors(app);
 }
