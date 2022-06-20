4a77d6283f10b15e7cf9cdf2986f3aef5ea1c8ff
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-06-20 20:29:33 -0300

    refactor: rename database driver variable

--- src/infra/database/prisma/index.ts
+++ src/infra/database/prisma/index.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const connectionString = `${process.env.DATABASE_DRIV}://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+const connectionString = `${process.env.DATABASE_DRVR}://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
 
 export const prisma = new PrismaClient({
   datasources: {
