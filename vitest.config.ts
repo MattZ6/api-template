48c1fdc0a8b439ce8faec16a8d6a6c13e17ea5c2
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-01-30 16:13:51 -0300

    refactor: migrate to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+function resolvePath(folder: string) {
+  return resolve(__dirname, 'src', folder);
+}
+
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@domain', replacement: resolvePath('domain') },
+      { find: '@application', replacement: resolvePath('application') },
+      { find: '@infra', replacement: resolvePath('infra') },
+      { find: '@presentation', replacement: resolvePath('presentation') },
+      { find: '@main', replacement: resolvePath('main') },
+    ],
+  },
+  test: {
+    clearMocks: true,
+    include: ['**/*.spec.ts'],
+    coverage: {
+      include: ['src/**/*.ts'],
+    },
+  },
+});
